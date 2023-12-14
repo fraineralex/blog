a330502981ce17c034f606e32ac7d25967379e91
Frainer Encarnación <fraineralex2001@gmail.com>
2023-12-13 23:18:16 -0400

    wip: add the sass dev dependency and update the layout

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 const withMDX = require('@next/mdx')()
+const path = require('path')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  }
 }
 
 module.exports = withMDX(nextConfig)
