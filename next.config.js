de07adc769d1db94cb7fe6c20f7243564cbb52bf
Frainer Encarnación <fraineralex2001@gmail.com>
2023-12-17 13:40:16 -0400

    fix: upgrade the @types/debug version from 4.0.0 to 4.1.0

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const { withContentlayer } = require('next-contentlayer')
-//const withMDX = require('@next/mdx')()
 const path = require('path')
 
 /** @type {import('next').NextConfig} */
@@ -7,11 +6,7 @@ const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx', 'md'],
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-  },
-  reactStrictMode: true, swcMinify: true,
-  experimental: {
-		mdxRs: true,
-	},
+  }
 }
 
 module.exports = withContentlayer(nextConfig)
