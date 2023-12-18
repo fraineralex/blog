f3797200a0d6db1d5ddc42169ed869a727d755b3
Frainer Encarnación <fraineralex2001@gmail.com>
2023-12-18 11:16:55 -0400

    fix: delete the rehype dependencies

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,20 +1,4 @@
 import { defineDocumentType, makeSource } from 'contentlayer/source-files'
-import remarkGfm from 'remark-gfm'
-import rehypePrettyCode from 'rehype-pretty-code'
-import rehypeSlug from 'rehype-slug'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-
-/** @type {import('contentlayer/source-files').ComputedFields} */
-const computedFields = {
-  path: {
-    type: 'string',
-    resolve: (doc: any) => `/${doc._raw.flattenedPath}`
-  },
-  slug: {
-    type: 'string',
-    resolve: (doc: any) => doc._raw.flattenedPath.split('/').slice(1).join('/')
-  }
-}
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
@@ -50,6 +34,17 @@ export const Post = defineDocumentType(() => ({
         type: 'string'
       }
     }
+  },
+  computedFields: {
+    path: {
+      type: 'string',
+      resolve: (doc: any) => `/${doc._raw.flattenedPath}`
+    },
+    slug: {
+      type: 'string',
+      resolve: (doc: any) =>
+        doc._raw.flattenedPath.split('/').slice(1).join('/')
+    }
   }
 }))
 
@@ -74,42 +69,21 @@ export const Page = defineDocumentType(() => ({
     pubDate: {
       type: 'date'
     }
+  },
+  computedFields: {
+    path: {
+      type: 'string',
+      resolve: (doc: any) => `/${doc._raw.flattenedPath}`
+    },
+    slug: {
+      type: 'string',
+      resolve: (doc: any) =>
+        doc._raw.flattenedPath.split('/').slice(1).join('/')
+    }
   }
 }))
 
 export default makeSource({
   contentDirPath: './content',
-  documentTypes: [Page, Post],
-  mdx: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [
-      rehypeSlug,
-      [
-        rehypePrettyCode as any,
-        {
-          theme: 'github-dark',
-          onVisitLine (node: any) {
-            if (node.children.length === 0) {
-              node.children = [{ type: 'text', value: ' ' }]
-            }
-          },
-          onVisitHighlightedLine (node: any) {
-            node.properties.className.push('line--highlighted')
-          },
-          onVisitHighlightedWord (node: any) {
-            node.properties.className = ['word--highlighted']
-          }
-        }
-      ],
-      [
-        rehypeAutolinkHeadings,
-        {
-          properties: {
-            className: ['subheading-anchor'],
-            ariaLabel: 'Link to section'
-          }
-        }
-      ]
-    ]
-  }
+  documentTypes: [Page, Post]
 })
