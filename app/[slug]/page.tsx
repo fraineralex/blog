39a8a96170fa0df2f684cac696129aa15758b9ec
Frainer Encarnación <fraineralex2001@gmail.com>
2024-01-06 18:23:49 -0400

    feat: add the home, the content page and the search posts features

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,7 +1,26 @@
 import { notFound } from 'next/navigation'
-import { allPosts } from 'contentlayer/generated'
-import Navbar from '../components/navbar'
-import BlogPost from '../components/blog-post'
+import { Mdx } from '@/app/components/articles/mdx'
+import { Header } from '../components/content/header'
+import '../components/content/mdx.css'
+import { ReportView } from '../components/content/view'
+import { Redis } from '@upstash/redis'
+import Image from 'next/image'
+import { Metadata, ResolvingMetadata } from 'next'
+import Link from 'next/link'
+import Subscribe from '../components/footer/subscribe'
+import { Post } from 'contentlayer/generated'
+
+let allPosts: Array<Post>
+
+if (process.env.NODE_ENV === 'development') {
+  import('../../util/monks').then(module => {
+    allPosts = module.allPostsDev
+  })
+} else {
+  import('contentlayer/generated').then(module => {
+    allPosts = module.allPosts
+  })
+}
 
 export const revalidate = 60
 
@@ -11,28 +30,110 @@ type Props = {
   }
 }
 
+const redis = Redis.fromEnv()
+
 export async function generateStaticParams (): Promise<Props['params'][]> {
   return allPosts
-    .filter(p => p.published)
+    .filter(post => post.published)
     .map(p => ({
       slug: p.slug
     }))
 }
 
+export async function generateMetadata (
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const slug = params?.slug
+  const post = allPosts.find(post => post.slug === slug)
+
+  if (!post) {
+    return {
+      title: 'Not Found'
+    }
+  }
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || []
+
+  return {
+    title: post.title,
+    openGraph: {
+      images: [post.hero, ...previousImages]
+    }
+  }
+}
+
 export default async function PostPage ({ params }: Props) {
   const slug = params?.slug
-  const post = allPosts.find((post) => post.slug === slug)
+  const post = allPosts.find(post => post.slug === slug)
 
   if (!post) {
     notFound()
   }
 
+  const views =
+    (await redis.get<number>(['pageviews', 'posts', slug].join(':'))) ?? 0
+
   return (
-    <body>
-      <Navbar />
-      <main className='py-12 lg:py-20'>
-        <BlogPost post={post} />
-      </main>
-    </body>
+    <>
+      <section className='min-h-screen max-w-4xl mx-auto px-8 text-zinc-300'>
+        <Header post={post} views={views} />
+        <ReportView slug={post.slug} />
+        <header className='mx-auto max-w-3xl text-center content pt-20 sm:pt-28'>
+          <h1 className='text-white mb-8'>{post.title}</h1>
+        </header>
+
+        <Image
+          className='rounded-xl mx-auto'
+          style={{ minWidth: '80%' }}
+          loading='lazy'
+          src={post.hero}
+          alt={post.title}
+          width={800}
+          height={427}
+        />
+
+        <small className='text-zinc-400 flex flex-col font-bold uppercase sm:flex-row text-center pb-8 place-content-center py-6 px-6'>
+          <span className='mb-2 sm:mb-0'>
+            {post.updated && <span>Updated on</span>}{' '}
+            <time
+              dateTime={new Date(post.updated || post.date).toISOString()}
+              className={`${
+                post.updated ? 'underline underline-offset-4' : undefined
+              }`}
+            >
+              {Intl.DateTimeFormat(undefined, {
+                dateStyle: 'medium'
+              }).format(new Date(post.updated || post.date))}
+            </time>{' '}
+            <span className='px-4'>•</span>
+            <span>{post.readTime} min read</span>
+          </span>
+          <span className='hidden px-4 sm:block'>|</span>
+          <div className='flex flex-wrap'>
+            {post.tags &&
+              post.tags.map((tag, index) => (
+                <div key={index}>
+                  <Link
+                    href={`/tags/${tag.value}`}
+                    className='text-hot-pink font-bold underline underline-offset-4 py-3 px-1 hover:text-white'
+                  >
+                    {tag.label}
+                  </Link>
+                  {index !== (post.tags?.length ?? 0) - 1 && (
+                    <span className='px-1'>•</span>
+                  )}
+                </div>
+              ))}
+          </div>
+        </small>
+
+        <article className='px-6 content pb-12'>
+          <Mdx code={post.body.code} />
+        </article>
+      </section>
+      <Subscribe />
+    </>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,193 @@
-import Navbar from './components/navbar'
-import Footer from './components/footer/footer'
-import HomeHeader from './components/home-header'
-import ArticleCard from './components/article-card'
-import { Post, allPosts } from 'contentlayer/generated'
-import { Key } from 'react'
+import Link from 'next/link'
+import React from 'react'
+import { Navigation } from './components/nav/nav'
+import { Article } from './components/articles/article'
+import { Redis } from '@upstash/redis'
+import { Eye } from 'lucide-react'
+import Image from 'next/image'
+import { Post } from 'contentlayer/generated'
+
+let allPosts: Array<Post>
+
+if (process.env.NODE_ENV === 'development') {
+  import('../util/monks').then(module => {
+    allPosts = module.allPostsDev
+  })
+} else {
+  import('contentlayer/generated').then(module => {
+    allPosts = module.allPosts
+  })
+}
+const redis = Redis.fromEnv()
 
 export const revalidate = 60
-
-export default function Home () {
-  const topPosts = allPosts
-    .filter(post => post.published)
-    .sort(
-      (a: Post, b: Post) =>
-        new Date(b.pubDate ?? Number.POSITIVE_INFINITY).valueOf() -
-        new Date(a.pubDate ?? Number.POSITIVE_INFINITY).valueOf()
+export default async function BlogPage () {
+  const views = (
+    await redis.mget<number[]>(
+      ...allPosts.map(post => ['pageviews', 'posts', post?.slug].join(':'))
     )
-    .slice(0, 3)
+  ).reduce((acc, v, i) => {
+    acc[allPosts[i].slug] = v ?? 0
+    return acc
+  }, {} as Record<string, number>)
 
-  const title = 'Top Articles'
-  console.log(topPosts.length)
+  const featured = allPosts.find(
+    post => post?.slug === 'installing-go-on-a-mac'
+  )!
+  const top2 = allPosts.find(post => post?.slug === 'installing-go-on-a-mac')!
+  const top3 = allPosts.find(post => post?.slug === 'installing-go-on-a-mac')!
+  const sorted = allPosts
+    .filter(post => post.published)
+    .filter(
+      post =>
+        post?.slug !== featured.slug &&
+        post?.slug !== top2?.slug &&
+        post?.slug !== top3?.slug
+    )
+    .sort(
+      (a, b) =>
+        new Date(b.date ?? Number.POSITIVE_INFINITY).getTime() -
+        new Date(a.date ?? Number.POSITIVE_INFINITY).getTime()
+    )
+
+  sorted.push(featured, top2, top3, featured)
+
+  const thereAreFourPosts = allPosts.length >= 4
 
   return (
-    <body className='bg-white text-black font-body leading-normal personality-casual'>
-      <Navbar />
-      <main className='py-12 lg:py-20'>
-        <article className='max-w-6xl mx-auto px-3'>
-          <HomeHeader title={title} />
-          <section
-            className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-8'
-            data-test='articles-section'
-          >
-            {topPosts.length > 0 &&
-              topPosts.map((post: Post, index: Key) => (
-                <div className='col-span-1' key={index}>
-                  <ArticleCard post={post} />
-                </div>
-              ))}
-          </section>
-        </article>
-      </main>
+    <div className='relative pb-16'>
+      <Navigation />
+      <div className='px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-26'>
+        <header className='mx-auto max-w-2xl text-center content'>
+          <h1 className='font-bold text-zinc-100 font-londrina'>
+            {thereAreFourPosts ? 'Blog Posts' : 'Articles'}
+          </h1>
+          <p className='text-zinc-400 -mt-20 text-lg leading-relaxed'>
+            Some of my thoughts on software, technology, and life.
+          </p>
+        </header>
 
-      <section className='flex space-x-8 justify-center text-xl pt-8 font-bold'>
-        <a href='/posts' className='mr-8' data-test='see-all-link'>
-          See All<span className='squiggle'>&rarr;</span>
-        </a>
-      </section>
-      <Footer />
-    </body>
+        {thereAreFourPosts && (
+          <>
+            <div className='mx-auto lg:mx-0 w-full'>
+              <h2 className='text-3xl font-bold tracking-wide text-zinc-500 sm:text-4xl pb-5 font-londrina'>
+                Top Articles
+              </h2>
+              <div className='w-full h-px bg-zinc-700' />
+            </div>
+
+            <div className='grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2'>
+              <Link
+                href={`/${featured.slug}`}
+                className='bg-gradient-to-br opacity-100  via-zinc-100/10 overflow-hidden relative border rounded-xl hover:bg-zinc-800/10 group md:gap-8 hover:border-zinc-400/50 border-zinc-600'
+              >
+                <Image
+                  style={{
+                    width: '100%',
+                    aspectRatio: '15 / 5',
+                    objectFit: 'cover'
+                  }}
+                  className='squiggle'
+                  src={featured.hero}
+                  alt={featured.title}
+                  width='360'
+                  height='192'
+                />
+                <article className='relative w-full h-full p-4 md:p-8'>
+                  <div className='flex items-center justify-between gap-2'>
+                    <div className='text-xs text-zinc-100'>
+                      {featured.date ? (
+                        <time dateTime={new Date(featured.date).toISOString()}>
+                          {Intl.DateTimeFormat(undefined, {
+                            dateStyle: 'medium'
+                          }).format(new Date(featured.date))}
+                        </time>
+                      ) : (
+                        <span>SOON</span>
+                      )}
+                    </div>
+                    <span className='flex items-center gap-1 text-xs text-zinc-500'>
+                      <Eye className='w-4 h-4' />{' '}
+                      {Intl.NumberFormat('en-US', {
+                        notation: 'compact'
+                      }).format(views[featured.slug] ?? 0)}
+                    </span>
+                  </div>
+
+                  <h2
+                    id='featured-post'
+                    className='mt-4 text-3xl sm:text-4xl font-bold text-zinc-100 group-hover:text-white  font-londrina'
+                  >
+                    {featured.title}
+                  </h2>
+                  <p className='mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300'>
+                    {featured.description}
+                  </p>
+                  <div className='absolute bottom-52'>
+                    <p className='hidden text-zinc-200 hover:text-zinc-50 lg:block'>
+                      Just {featured.readTime} min read{' '}
+                      <span aria-hidden='true'>&rarr;</span>
+                    </p>
+                  </div>
+                </article>
+              </Link>
+
+              <div className='flex flex-col w-full gap-9 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 '>
+                {[top2, top3].map((post, index) => (
+                  <Article
+                    key={post?.slug || index}
+                    post={post}
+                    views={views[post?.slug] ?? 0}
+                    isTopArticle
+                  />
+                ))}
+              </div>
+            </div>
+            <div className='mx-auto lg:mx-0 w-full'>
+              <h2 className='text-3xl font-bold tracking-wide text-zinc-500 sm:text-4xl pb-5 font-londrina'>
+                Latest Articles
+              </h2>
+              <div className='w-full h-px bg-zinc-700' />
+            </div>
+          </>
+        )}
+
+        <div className='grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3'>
+          <div className='grid grid-cols-1 gap-4'>
+            {sorted
+              .filter((_, i) => i % 3 === 0)
+              .map((post, index) => (
+                <Article
+                  key={post?.slug || index}
+                  post={post}
+                  views={views[post?.slug] ?? 0}
+                />
+              ))}
+          </div>
+          <div className='grid grid-cols-1 gap-4'>
+            {sorted
+              .filter((_, i) => i % 3 === 1)
+              .map((post, index) => (
+                <Article
+                  key={post?.slug || index}
+                  post={post}
+                  views={views[post?.slug] ?? 0}
+                />
+              ))}
+          </div>
+          <div className='grid grid-cols-1 gap-4'>
+            {sorted
+              .filter((_, i) => i % 3 === 2)
+              .map((post, index) => (
+                <Article
+                  key={post?.slug || index}
+                  post={post}
+                  views={views[post?.slug] ?? 0}
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
