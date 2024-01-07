63a5547d4643c10bf4c109eecdb9ee2c8734e932
Frainer Encarnación <fraineralex2001@gmail.com>
2024-01-07 00:37:41 -0400

    feat: add an alert to notify that the newsletter feature is not yet implemented

--- app/components/footer/subscribe.tsx
+++ app/components/footer/subscribe.tsx
@@ -1,11 +1,33 @@
+'use client'
+
+import { useState } from 'react'
+import { NewsLetterAlert } from './newsletter-alert'
+
 export default function Subscribe () {
+  const [alert, setAlert] = useState(false)
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const email = formData.get('email')
+    setAlert(true)
+    // wait for 5 seconds
+    setTimeout(() => {
+      setAlert(false)
+    }, 5000)
+  }
+
   return (
     <aside className='text-center py-16 max-w-3xl mx-auto'>
+      {alert && <NewsLetterAlert setAlert={setAlert} />}
       <div className='w-full border-t-2 mb-8 border-zinc-200 squiggle'></div>
       <h3 className='font-londrina text-4xl uppercase font-bold leading-none text-zinc-100'>
         Subscribe to the newsletter
       </h3>
-      <form className='mt-8 flex flex-wrap items-center md:w-2/3 mx-auto'>
+      <form
+        className='mt-8 flex flex-wrap items-center md:w-2/3 mx-auto'
+        onSubmit={handleSubmit}
+      >
         <input
           name='email'
           className='w-full flex-1 bg-zinc-500 py-3 px-6 rounded text-white border-zinc-500'
